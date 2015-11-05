c9774b22b850ec2bf637af37b2940428c6af37ea
agarmisa <alisongarmisa@gmail.com>
2015-11-05 07:03:55 -0500

    clinic model test

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,2 +1,6 @@
 class Clinic < ActiveRecord::Base
+  validates :name, presence: true
+  validates :website, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
 end
