2ab1a61e173c242d37334db6d1a9d96ea1cd7c02
agarmisa <alisongarmisa@gmail.com>
2015-11-05 06:22:49 -0500

    added sample clinic

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Clinic.create(name: 'MGH Medical Walk In Unit', website: 'http://www.yelp.com/biz/mgh-medical-walk-in-unit-boston?osq=Walk+In+Clinics', address: '15 Parkman St Boston, MA 02114', phone: '(617) 726-2707')
